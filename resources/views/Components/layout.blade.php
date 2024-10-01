f17a1d98c3b453b8a85e92655b0a89543e22e888
USER <vdiocampo009@gmail.com>
2024-10-01 17:06:44 +0800

    10-1_Vid3_Create-the-Obligatory-Three-Page-Layout-Using-Layout-Files

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
+    <title>Home Page</title>
 </head>
 <body>
-  <h1>Hello Wassup My Frendo</h1> 
+    <nav>
+    <a href="/">Home</a>
+    <a href="/contact">About</a>
+    <a href="/about">Contact</a>
+    </nav>
+
+    {{ $slot }}
 </body>
 </html>
