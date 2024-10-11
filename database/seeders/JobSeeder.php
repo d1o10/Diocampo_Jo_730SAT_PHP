df92dc790e27e7cc6f1ed062488514dd6e83b1e3
USER <vdiocampo009@gmail.com>
2024-10-12 01:40:47 +0800

    10-2_Vid16_Forms-and-csrf-explained-with-examples

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Job;
 use Illuminate\Database\Seeder;
 
 class JobSeeder extends Seeder
@@ -12,6 +12,6 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Job::factory(200)->create();
     }
 }
